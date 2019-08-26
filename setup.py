1ed39c00e0c26caf59e2e1869a8283a70a0c6d50
Johann Zelger <j.zelger@techdivision.com>
2019-08-26 15:49:22 +0200

    added package_dir to be root

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name='valet-sh-cli',
     version='1.0',
+    package_dir={''},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
