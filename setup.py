8b998e889a903cb29b97cb63b8c1a043eca9213f
Johann Zelger <j.zelger@techdivision.com>
2019-08-26 15:50:45 +0200

    try

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='valet-sh-cli',
     version='1.0',
-    package_dir={'/usr/share/valet-sh'},
+    package_dir={'./'},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
