bd6fb8c4435518be6cc9fc59097c4188bb87ce05
Johann Zelger <j.zelger@techdivision.com>
2019-08-26 15:39:48 +0200

    removed packages

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.core import setup
 setup(
     name='valet-sh-cli',
     version='1.0',
-    packages=['valet-sh',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
