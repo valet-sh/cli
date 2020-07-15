ff571e25253139a5596bc6983c42300c3ca5408f
Johann Zelger <j.zelger@techdivision.com>
2020-07-15 21:42:11 +0200

    raised version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='valet-sh-cli',
-    version='1.1',
+    version='1.1.1',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     scripts=['valet.sh']
