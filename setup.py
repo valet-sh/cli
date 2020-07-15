28d5139af3c60417b1e8b321469e01083c845024
Johann Zelger <j.zelger@techdivision.com>
2020-07-15 21:48:55 +0200

    fixed setup.py description

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='valet-sh-cli',
     author='Johann Zelger',
     author_email='j.zelger@techdivision.com',
-    summary='valet.sh command line interface written in bash',
+    description='valet.sh command line interface written in bash',
     url='https://github.com/valet-sh/cli',
     maintainer='Johann Zelger',
     maintainer_email='j.zelger@techdivision.com',
-    version='1.1.2',
+    version='1.1.3',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     scripts=['valet.sh']
